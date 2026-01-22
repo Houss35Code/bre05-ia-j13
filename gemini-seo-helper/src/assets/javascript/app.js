f98f6b2eeba2d58906c0a8e893843d59a83afa8b
Houss35Code <h.halifa035@gmail.com>
2026-01-22 12:06:45 +0100

    Dernières étapes du projet

---
+++ gemini-seo-helper/src/assets/javascript/app.js
@@ -0,0 +1,6 @@
+// Étape 4 : instancier le formulaire une fois le DOM chargé
+import { ContentForm } from "./form.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+    let form = new ContentForm();
+});
